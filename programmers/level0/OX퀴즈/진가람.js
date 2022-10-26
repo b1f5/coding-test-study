5b16bc48cbe52eb2034e5d490204d4b23b449f02
Jin garam <2001garam@naver.com>
2022-10-26 18:28:59 +0900

    ✨Add OX quiz

--- programmers/level0/OX퀴즈/진가람.js
+++ programmers/level0/OX퀴즈/진가람.js
@@ -1,1 +1,34 @@
-console.log('hello world');
+/**
+ *
+ * @param {string[]} quiz +, - 연산자가 포함된 수식 목록
+ * @returns {string[]} 수식이 옳은지 O,X 반환
+ */
+function solution(quiz) {
+  return quiz.map((e) => {
+    let splited = e.split(' ');
+    let [a, operator, b, , result] = splited;
+    return calculate(Number(a), Number(b), operator) === Number(result)
+      ? 'O'
+      : 'X';
+  });
+}
+
+/**
+ *
+ * @param {number} a 0부터 9까지 숫자로 이루어진 정수. 음수일 수도 있다.
+ * @param {number} b 0부터 9까지 숫자로 이루어진 정수. 음수일 수도 있다.
+ * @param {string} operator +, - 중 하나
+ * @returns
+ */
+function calculate(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    default:
+      throw new Error('Invalid operator');
+  }
+}
+
+console.log(solution(['3 - 4 = -3', '5 + 6 = 11']));
