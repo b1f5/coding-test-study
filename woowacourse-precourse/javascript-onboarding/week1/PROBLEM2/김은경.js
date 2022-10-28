dcd0da9b25aba36d67dfb766f506fc9bac4261e9
ekkim <eklikeable@kakao.com>
2022-10-28 16:06:07 +0900

    🎨 주석 및 정규표현식 수정 \w → [a-z]

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM2/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM2/김은경.js
@@ -1,18 +1,17 @@
 function problem2(cryptogram) {
-  let pattern = /(\w)\1+/g;
+  let pattern = /([a-z])\1+/g;
   let decode = cryptogram;
+  // 더이상 중복되는 문자열이 없을 때 까지 중복문자 제거를 반복
   while (pattern.test(decode)) {
     decode = decode.replace(pattern, '');
-    console.log(decode);
   }
+  // 중복문자열 제거가 완료된 변수 decode를 리턴, 남아있는 문자열이 없는경우 공백을 리턴한다.
   return decode ? decode : '';
 }
 
 module.exports = problem2;
 
-// /(\w)\1/
-// (\w) : 첫번째 캡쳐그룹
-// \w : 63개 문자에 일치 ([a-zA-Z0-9_] 와 같음)
-// \1~9 : 정규식 내 1~9번째 캡쳐그룹 참조
-// + : 1회 이상 연속으로 반복되는 문자에 가능한 많이 일치, {1,}와 동일
-// g : 모든 문자와 여러줄 일치(global)
+// 정규표현식 패턴 : /([a-z])\1+/g
+// 문자열은 알파벳 소문자로만 이루어져 있으므로 [a-z]로 범위를 지정
+// \1+ : 캡쳐그룹([a-z])와 같은 문자가 1개 또는 그 이상 이어진 경우를 찾음
+// g : 조건에 해당하는 모든 값을 찾음
