03cd8497679e04e057d5c69ddcecb45387c3887c
Chun-gu <chungu.yi92@gmail.com>
2022-10-28 16:01:37 +0900

    ♻️ ts-check 추가하고 정규표현식 개선에 따른 리팩토링

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM2/이춘구.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM2/이춘구.js
@@ -1,29 +1,27 @@
+// @ts-check
 /**
  * 암호를 해독해서 반환하는 함수
  * @param {string} cryptogram 암호
  * @returns {string} 해독 결과
  */
 function problem2(cryptogram) {
-  // 암호를 한 글자씩 나눈다.
-  const characters = cryptogram.split("");
+  // 현재 글자가 두 번 이상 연속으로 반복되는지 검사할 정규표현식을 만든다.
+  const ConsecutiveDuplicationPattern = /([a-z])\1+/;
 
-  // 한 글자씩 순회하면서
-  for (const character of characters) {
-    // 현재 글자가 두 번 이상 연속으로 반복되는지 검사할 정규표현식을 만든다.
-    const ConsecutiveDuplication = new RegExp(`${character}{2,}`);
-    // 연속하는 중복 문자가 있는지 검사한다.
-    const hasConsecutiveDuplication = ConsecutiveDuplication.test(cryptogram);
+  // 연속하는 중복 문자가 있는지 검사한다.
+  const hasConsecutiveDuplication =
+    ConsecutiveDuplicationPattern.test(cryptogram);
 
-    // 연속하는 중복 문자가 있다면
-    if (hasConsecutiveDuplication === true) {
-      // 연속하는 문자열을 제거한 문자열을 만들고,
-      const replacedCryptogram = cryptogram.replace(ConsecutiveDuplication, "");
-      // 재귀 반복한다.
-      return problem2(replacedCryptogram);
-    }
+  // 연속하는 중복 문자가 있다면
+  if (hasConsecutiveDuplication) {
+    // 연속하는 문자열을 제거한 문자열을 만들고,
+    const replacedCryptogram = cryptogram.replace(
+      ConsecutiveDuplicationPattern,
+      ""
+    );
+    // 재귀 반복한다.
+    return problem2(replacedCryptogram);
   }
 
   return cryptogram;
 }
-
-module.exports = problem2;
