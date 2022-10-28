6c42c4adef7c39801cc5464b0939bab4f1efe545
ekkim <eklikeable@kakao.com>
2022-10-28 16:15:03 +0900

    🎨 변수명(targetWord→targetASCII), 메소드(reduce→join)변경

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM4/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM4/김은경.js
@@ -7,21 +7,21 @@ function problem4(word) {
       result.push(' ');
     } else {
       // 문자열이 공백이 아니라면
-      const targetWord = e.charCodeAt();
+      const targetASCII = e.charCodeAt();
       let replaceWord = '';
       //문자열이 대문자인 경우 대체문자 찾기
-      if (targetWord >= 65 && targetWord <= 90) {
-        replaceWord = String.fromCharCode(90 - (targetWord - 65));
+      if (targetASCII >= 65 && targetASCII <= 90) {
+        replaceWord = String.fromCharCode(90 - (targetASCII - 65));
         //문자열이 소문자인 경우 대체문자 찾기
-      } else if (targetWord >= 97 && targetWord <= 122) {
-        replaceWord = String.fromCharCode(122 - (targetWord - 97));
+      } else if (targetASCII >= 97 && targetASCII <= 122) {
+        replaceWord = String.fromCharCode(122 - (targetASCII - 97));
       }
       // 찾은 대체문자를 result 배열에 담아줌
       result.push(replaceWord);
     }
   }
   // result 배열을 문자열로 리턴
-  return result.reduce((sum, cur) => sum + cur, '');
+  return result.join('');
 }
 
 module.exports = problem4;
