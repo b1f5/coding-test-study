3d8efa12641c6153f811715fef5a60a67ee155b0
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-29 11:58:07 +0900

    ♻️ 네이밍 변경

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM4/곽성재.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM4/곽성재.js
@@ -1,15 +1,16 @@
 function problem4(word) {
+  // 변수명....
   let answer = "";
   // 1) 모든철자를 대소문자 구분하여 만드는 과정
-  const alphabet_L = "abcdefghijklmnopqrstuvwxyz";
-  const alphabet_U = alphabet_L.toUpperCase();
+  const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+  const UPPER_CASE = LOWER_CASE.toUpperCase();
 
   // 3) 순회하면서 대소문자에 맞게 변환하는 과정
   for (let spell of word) {
-    if (alphabet_L.includes(spell)) {
-      answer += convert(spell, alphabet_L);
-    } else if (alphabet_U.includes(spell)) {
-      answer += convert(spell, alphabet_U);
+    if (LOWER_CASE.includes(spell)) {
+      answer += convert(spell, LOWER_CASE);
+    } else if (UPPER_CASE.includes(spell)) {
+      answer += convert(spell, UPPER_CASE);
     } else {
       // 4) 알파벳 외의 문자는 변환하지 않고 그대로 넣어주는 과정
       answer += spell;
@@ -21,6 +22,10 @@ function problem4(word) {
 
 // 2) 청개구리 언어로 변환하는 과정
 function convert(spell, fullSpell) {
-  const idx = 25 - fullSpell.indexOf(spell);
-  return fullSpell[idx];
+  const LENGTH_OF_FULL = fullSpell.length;
+  const IDX_CHANGE =
+    LENGTH_OF_FULL - (fullSpell.indexOf(spell) + 1);
+  return fullSpell[IDX_CHANGE];
 }
+
+console.log(problem4("I love you"));
