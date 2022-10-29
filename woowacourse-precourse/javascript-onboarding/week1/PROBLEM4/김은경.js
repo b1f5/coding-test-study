7417ad84eff712bd1fc67cea773b5e345cb420ab
ekkim <eklikeable@kakao.com>
2022-10-29 22:52:28 +0900

    :sparkle: 함수 분리, 입력값이 알파벳이 아닌경우 예외 처리

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM4/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM4/김은경.js
@@ -1,27 +1,32 @@
+// convertToFrogDictionary : 영문 대소문자의 ASCII CODE를 한글자씩 입력 받아 반대로 바꿔 리턴하는 함수
+function convertToFrogDictionary(targetChar) {
+  //문자열이 대문자(65~90)인 경우 대체문자 찾기
+  if (targetChar >= 65 && targetChar <= 90) {
+    return String.fromCharCode(90 - (targetChar - 65));
+    //문자열이 소문자(97~122)인 경우 대체문자 찾기
+  } else if (targetChar >= 97 && targetChar <= 122) {
+    return String.fromCharCode(122 - (targetChar - 97));
+  }
+}
+
 function problem4(word) {
   const words = word.split('');
   const result = [];
-  for (e of words) {
-    if (e === ' ') {
-      // 문자열이 공백이면 result배열에 공백을 담아줌
-      result.push(' ');
+  for (char of words) {
+    const pattern = /[a-zA-Z]/;
+    // char가 알파벳이라면
+    if (pattern.test(char)) {
+      // char를 ASCII CODE로 변환
+      const targetASCII = char.charCodeAt();
+      // convertToFrogDictionary 함수를 실행하여, 리턴값을 result에 푸시
+      result.push(convertToFrogDictionary(targetASCII));
     } else {
-      // 문자열이 공백이 아니라면
-      const targetASCII = e.charCodeAt();
-      let replaceWord = '';
-      //문자열이 대문자인 경우 대체문자 찾기
-      if (targetASCII >= 65 && targetASCII <= 90) {
-        replaceWord = String.fromCharCode(90 - (targetASCII - 65));
-        //문자열이 소문자인 경우 대체문자 찾기
-      } else if (targetASCII >= 97 && targetASCII <= 122) {
-        replaceWord = String.fromCharCode(122 - (targetASCII - 97));
-      }
-      // 찾은 대체문자를 result 배열에 담아줌
-      result.push(replaceWord);
+      // char가 알파벳이 아니라면, 변환하지 않고 그대로 result에 푸시
+      result.push(char);
     }
   }
   // result 배열을 문자열로 리턴
   return result.join('');
 }
 
-module.exports = problem4;
+export default problem4;
