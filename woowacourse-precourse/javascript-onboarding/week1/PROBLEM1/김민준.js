69f0d056a2e0753dc7a99089681336c2650202e2
mj0107 <alswns1285@gmail.com>
2022-10-31 00:58:16 +0900

    ♻️ 최대 점수를 구하는 부분 따로 함수로 빼서 구현

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM1/김민준.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM1/김민준.js
@@ -27,34 +27,13 @@ function getPageEachDigitMultiply(page) {
 }
 
 /**
- * 받은 페이지들이 룰에 어긋나지 않고 유효한 쌍인지 확인하는 함수
- *
- * @param {number[]} pobi pobi의 왼쪽, 오른쪽 페이지
- * @param {number[]} crong crong의 왼쪽, 오른쪽 페이지
- * @returns {boolean}
+ * pobi와 crong의 왼쪽 오른쪽 페이지 쌍을 받아 각각의 최대 점수를 return 하는 함수
+ * 
+ * @param {number[]} pobi pobi의 [왼쪽, 오른쪽] 페이지
+ * @param {number[]} crong crong의 [왼쪽, 오른쪽] 페이지
+ * @returns 
  */
-function isValidPage(pobi, crong) {
-  const PAGE_LIST = [...pobi, ...crong];
-  // 시작 면이나 마지막 면이 나올 경우
-  if (PAGE_LIST.includes(1) || PAGE_LIST.includes(400)) return false;
-
-  // 왼쪽과 오른쪽 페이지 쌍이 옳지 않을 경우
-  if (pobi[0] + 1 !== pobi[1]) return false;
-  if (crong[0] + 1 !== crong[1]) return false;
-
-  return true;
-}
-
-function solution() {
-  let pobi = [97, 98];
-  let crong = [197, 198];
-  // let pobi = [131, 132];
-  // let crong = [211, 212];
-  // let pobi = [99, 102];
-  // let crong = [211, 212];
-
-  if (isValidPage(pobi, crong) === false) return -1;
-
+function getMaxScore(pobi, crong) {
   const [POBI_LEFT_PAGE, POBI_RIGHT_PAGE] = pobi;
   const [CRONG_LEFT_PAGE, CRONG_RIGHT_PAGE] = crong;
 
@@ -84,12 +63,45 @@ function solution() {
     CRONG_RIGHT_PAGE_MAX_SCORE
   );
 
+  return { POBI_MAX_SCORE, CRONG_MAX_SCORE };
+}
+
+/**
+ * 받은 페이지들이 룰에 어긋나지 않고 유효한 쌍인지 확인하는 함수
+ *
+ * @param {number[]} pobi pobi의 왼쪽, 오른쪽 페이지
+ * @param {number[]} crong crong의 왼쪽, 오른쪽 페이지
+ * @returns {boolean}
+ */
+function isValidPage(pobi, crong) {
+  const PAGE_LIST = [...pobi, ...crong];
+  // 시작 면이나 마지막 면이 나올 경우
+  if (PAGE_LIST.includes(1) || PAGE_LIST.includes(400)) return false;
+
+  // 왼쪽과 오른쪽 페이지 쌍이 옳지 않을 경우
+  if (pobi[0] + 1 !== pobi[1]) return false;
+  if (crong[0] + 1 !== crong[1]) return false;
+
+  return true;
+}
+
+function solution() {
+  let pobi = [97, 98];
+  let crong = [197, 198];
+  // let pobi = [131, 132];
+  // let crong = [211, 212];
+  // let pobi = [99, 102];
+  // let crong = [211, 212];
+
+  if (isValidPage(pobi, crong) === false) return -1;
+
   let result = 0;
   const gameResultObj = {
     POBI_WIN: 1,
     CRONG_WIN: 2,
     DRAW: 0,
   };
+  const { POBI_MAX_SCORE, CRONG_MAX_SCORE } = getMaxScore(pobi, crong);
 
   if (POBI_MAX_SCORE > CRONG_MAX_SCORE) result = gameResultObj.POBI_WIN;
   else if (POBI_MAX_SCORE < CRONG_MAX_SCORE) result = gameResultObj.CRONG_WIN;
