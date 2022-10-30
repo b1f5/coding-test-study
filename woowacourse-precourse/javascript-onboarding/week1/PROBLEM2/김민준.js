662591058c445372e7e6f94d37bac9cbbf45661f
mj0107 <alswns1285@gmail.com>
2022-10-31 01:26:31 +0900

    🐛 같은글자가 3개 연속으로 올 경우 제거 안되는 문제 수정
    - 전글자와 다음글자 모두 비교해서 다를 경우 stack에 push
    - map을 forEach로 수정

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM2/김민준.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM2/김민준.js
@@ -4,17 +4,18 @@ function solution() {
   let preStackSize = 0;
   const recursion = (CRYPTOGRAM) => {
     let stack = [];
-    CRYPTOGRAM.split('').map(el => {
-      // 스택의 top과 같으면 stack에서 pop
-      if(el === stack.at(-1)) stack.pop();
-      // 아니라면 push
-      else stack.push(el);
+    let preLetter = '';
+    let nextLetter = '';
+    CRYPTOGRAM.split('').forEach((el, idx) => {
+      nextLetter = CRYPTOGRAM[idx + 1];
+      if(el !== preLetter && el !== nextLetter) stack.push(el); 
+      preLetter = el;
     });
 
     // 만약 이전 단계의 재귀의 스택의 사이즈와 같은 사이즈라면
     // 중복되어서 사라진것이 없다는 뜻이므로
     // return 해서 재귀 종료
-    if(stack.length === size) return stack.join('');
+    if(stack.length === preStackSize) return stack.join('');
     preStackSize = stack.length;
 
     // 중복 제거한 stack으로 다시 재귀
