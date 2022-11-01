3a76effc84a0723faac50b4bf8c20aab639a654b
ekkim <eklikeable@kakao.com>
2022-11-01 14:55:07 +0900

    🎨 함수 위치 변경, 주석 정리

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM4/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM4/김은경.js
@@ -1,32 +1,36 @@
-// convertToFrogDictionary : 영문 대소문자의 ASCII CODE를 한글자씩 입력 받아 반대로 바꿔 리턴하는 함수
-function convertToFrogDictionary(targetChar) {
-  //문자열이 대문자(65~90)인 경우 대체문자 찾기
-  if (targetChar >= 65 && targetChar <= 90) {
-    return String.fromCharCode(90 - (targetChar - 65));
-    //문자열이 소문자(97~122)인 경우 대체문자 찾기
-  } else if (targetChar >= 97 && targetChar <= 122) {
-    return String.fromCharCode(122 - (targetChar - 97));
-  }
-}
+/**
+ * 청개구리 사전을 참고하여 엄마 말씀을 반대로 변환
+ * - 알파벳 외의 문자는 변환하지 않는다
+ * @function problem4
+ * @param {string} word 청개구리 엄마 말씀
+ * @returns {string} result 변환된 문장
+ *  */
 
 function problem4(word) {
   const words = word.split('');
   const result = [];
   for (char of words) {
     const pattern = /[a-zA-Z]/;
-    // char가 알파벳이라면
     if (pattern.test(char)) {
-      // char를 ASCII CODE로 변환
       const targetASCII = char.charCodeAt();
-      // convertToFrogDictionary 함수를 실행하여, 리턴값을 result에 푸시
       result.push(convertToFrogDictionary(targetASCII));
     } else {
-      // char가 알파벳이 아니라면, 변환하지 않고 그대로 result에 푸시
       result.push(char);
     }
   }
-  // result 배열을 문자열로 리턴
   return result.join('');
+}
+
+/**
+ * @function convertToFrogDictionary
+ * @returns {string} 영문 대소문자의 ASCII CODE를 한글자씩 입력 받아 반대로 바꿔 리턴
+ *  */
+function convertToFrogDictionary(targetChar) {
+  if (targetChar >= 65 && targetChar <= 90) {
+    return String.fromCharCode(90 - (targetChar - 65));
+  } else if (targetChar >= 97 && targetChar <= 122) {
+    return String.fromCharCode(122 - (targetChar - 97));
+  }
 }
 
 module.exports = problem4;
