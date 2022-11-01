19c03883c0afb47dd9c0803aa64d7f244b702e8c
ekkim <eklikeable@kakao.com>
2022-11-01 14:54:11 +0900

    🎨 함수 위치 변경, 주석 정리

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM1/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM1/김은경.js
@@ -1,20 +1,12 @@
-// getMaximum : 배열로 받은 수 들을 더한 값과 곱한 값 중 최대값을 리턴하는 함수
-function getMaximum(array) {
-  let candidates = [];
-  for (let pageNumber of array) {
-    // pageNumber를 한자리씩 쪼개어 splitNumber에 담아줌
-    const splitNumber = pageNumber.toString().split('');
-    // 더하기 값 구하기
-    candidates.push(splitNumber.reduce((sum, cur) => sum + parseInt(cur), 0));
-    // 곱하기 값 구하기
-    candidates.push(splitNumber.reduce((sum, cur) => sum * parseInt(cur), 1));
-  }
-  // 더하기/곱하기 값 중 가장 큰 수 리턴
-  return Math.max(...candidates);
-}
-
+/**
+ * 포비와 크롱의 페이지 번호 게임
+ * - 책을 임의로 펼쳐 나온 페이지의 각 숫자를 더하거나 곱해 가장 큰 수를 비교
+ * @function problem1
+ * @param {string[]} pobi 포비가 펼친 페이지
+ * @param {string[]} crong 크롱이 펼친 페이지
+ * @returns {number} 포비가 이기면 1, 크롱이 이기면 2, 무승부는 0, 예외사항은 -1
+ *  */
 function problem1(pobi, crong) {
-  // 게임 결과에 따른 return 값 설정
   const gameResult = {
     EXCEPTION: -1,
     WINNER_POBI: 1,
@@ -22,11 +14,9 @@ function problem1(pobi, crong) {
     WINNER_TIE: 0,
   };
 
-  // 좌,우 페이지 번호가 이어지지 않는 경우 예외처리
   if (pobi[0] + 1 !== pobi[1] || crong[0] + 1 !== crong[1])
     return gameResult.EXCEPTION;
 
-  // pobi와 crong의 getMaximum 함수 리턴값 비교
   const pobiNum = getMaximum(pobi);
   const crongNum = getMaximum(crong);
   if (pobiNum === crongNum) return gameResult.WINNER_TIE;
@@ -35,6 +25,20 @@ function problem1(pobi, crong) {
   } else {
     return gameResult.WINNER_CRONG;
   }
+}
+
+/**
+ * @function getMaximum
+ * @returns {number} 배열로 받은 수 들을 더한 값과 곱한 값 중 최대값을 리턴
+ *  */
+function getMaximum(array) {
+  let candidates = [];
+  for (let pageNumber of array) {
+    const splitNumber = pageNumber.toString().split('');
+    candidates.push(splitNumber.reduce((sum, cur) => sum + parseInt(cur), 0));
+    candidates.push(splitNumber.reduce((sum, cur) => sum * parseInt(cur), 1));
+  }
+  return Math.max(...candidates);
 }
 
 module.exports = problem1;
