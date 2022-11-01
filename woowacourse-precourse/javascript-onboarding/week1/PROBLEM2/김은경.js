4f48df2cebec7f9cec3face712976b41a8f9ad2f
ekkim <eklikeable@kakao.com>
2022-11-01 14:54:23 +0900

    🎨 주석 정리

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM2/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM2/김은경.js
@@ -1,17 +1,17 @@
+/**
+ * 중복문자 암호문 해독
+ * - 3, 6, 9가 들어가는 개수만큼 손뼉을 쳐야 한다
+ * @function problem2
+ * @param {string} cryptogram 임의의 문자열
+ * @returns {string} 연속하는 중복 문자들을 삭제한 결과
+ *  */
 function problem2(cryptogram) {
   let pattern = /([a-z])\1+/g;
   let decode = cryptogram;
-  // 더이상 중복되는 문자열이 없을 때 까지 중복문자 제거를 반복
   while (pattern.test(decode)) {
     decode = decode.replace(pattern, '');
   }
-  // 중복문자열 제거가 완료된 변수 decode를 리턴, 남아있는 문자열이 없는경우 공백을 리턴한다.
   return decode;
 }
 
 module.exports = problem2;
-
-// 정규표현식 패턴 : /([a-z])\1+/g
-// 문자열은 알파벳 소문자로만 이루어져 있으므로 [a-z]로 범위를 지정
-// \1+ : 캡쳐그룹([a-z])와 같은 문자가 1개 또는 그 이상 이어진 경우를 찾음
-// g : 조건에 해당하는 모든 값을 찾음
