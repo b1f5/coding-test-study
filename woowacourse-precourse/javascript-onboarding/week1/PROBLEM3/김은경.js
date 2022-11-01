0e78389bc9c39d426781829d1a9b45e8eb376914
ekkim <eklikeable@kakao.com>
2022-11-01 14:54:52 +0900

    🎨 주석 정리

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM3/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM3/김은경.js
@@ -1,18 +1,10 @@
-// 첫번째 풀이 : 객체 생성 후, 2중 for문 사용하여 카운트
-
-function firstTry(number) {
-  const game369 = { 3: 0, 6: 0, 9: 0 };
-  for (let i = 1; i <= number; i++) {
-    let numbers = String(i).split('');
-    for (let e of numbers) {
-      if (e in game369) game369[e] += 1;
-    }
-  }
-  return Object.values(game369).reduce((sum, cur) => sum + cur, 0);
-}
-
-// 두번째 풀이 : 모든숫자를 배열에 담은 뒤 3,6,9의 갯수만 리턴
-
+/**
+ * 배달이가 좋아하는 3,6,9 게임
+ * - 3, 6, 9가 들어가는 개수만큼 손뼉을 쳐야 한다
+ * @function problem3
+ * @param {number} number 1부터 number까지 게임 진행
+ * @returns {number} 손뼉을 치는 횟수
+ *  */
 function problem3(number) {
   let numbers = [];
   for (let i = 1; i <= number; i++) {
