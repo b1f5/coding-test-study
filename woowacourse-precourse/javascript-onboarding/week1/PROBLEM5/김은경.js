f6fafa8f39b0d695a703baecc31237a6fdcb5029
ekkim <eklikeable@kakao.com>
2022-11-01 14:55:21 +0900

    🎨 주석 추가

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM5/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM5/김은경.js
@@ -1,3 +1,10 @@
+/**
+ * 돈 담을 지갑이 최대한 가볍도록 큰 금액의 화폐 위주로 받는다
+ * @function problem5
+ * @param {number} money 돈의 액수
+ * @returns {string[]} result 금액이 큰 순서대로 화폐의 갯수를 배열에 담아 리턴
+ *  */
+
 function problem5(money) {
   const CURRENCIES = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let result = Array.from({ length: 9 }, () => 0);
