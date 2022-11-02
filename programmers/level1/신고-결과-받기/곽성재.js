9bc9f033367298e7ad1bc9841c42c29bfd204e08
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-02 11:36:19 +0900

    ♻️ 신고결과에서 중복 제거

--- programmers/level1/신고-결과-받기/곽성재.js
+++ programmers/level1/신고-결과-받기/곽성재.js
@@ -7,7 +7,7 @@ function solution(id_list, report, k) {
   for (const suspended_user of suspended) {
     for (let j = 0; j < id_list.length; j++) {
       const id = id_list[j];
-      if (reportBoard[id].has(suspended_user)) {
+      if (reportBoard[id].includes(suspended_user)) {
         answer[j]++;
       }
     }
@@ -17,30 +17,31 @@ function solution(id_list, report, k) {
 
 // {
 //   muzi : {neo, frodo} // Set자료형
-//   neo : {neo, frodo} // Set자료형
-//   frodo : {neo, frodo} // Set자료형
-//   con : {neo, frodo} // Set자료형
+// }
+
+// {
+//   muzi : [neo, frodo] // 배열로 변경
 // }
 
 // key값은 id로, value값은 Set으로 가지는 객체를 만들고 초기화한다
 // Set인 이유는 한 유저가 여러번 신고해도 1회로 카운팅되어서
+
+// 위의 코드였지만, 아예 리포트를 셋으로 돌리면 가능하다
 function makeReportBoard(id_list, report) {
+  let deduplication = new Set(report);
   const reportBoard = new Object();
   for (const id of id_list) {
-    reportBoard[id] = new Set();
+    reportBoard[id] = [];
   }
-  for (const i of report) {
+  for (const i of deduplication) {
     const [reporter, reported] = i.split(" ");
-    reportBoard[reporter].add(reported);
+    reportBoard[reporter].push(reported);
   }
   return reportBoard;
 }
 
 function calculateSuspendedUser(id_list, reportBoard, k) {
-  let temp = [];
-  for (const id of id_list) {
-    temp = [...temp, ...reportBoard[id]];
-  }
+  let temp = [...Object.values(reportBoard).flat()];
   const suspended = [];
   for (const id of id_list) {
     if (temp.filter((el) => el === id).length >= k) {
