c0f93a3079d006a976f28efddc33bc2ceefdba99
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-11-02 21:25:03 +0900

    :recycle: removed console.log, fixed answer type from array to string

--- woowacourse-precourse/javascript-onboarding/week1/PROBLEM1/전세원.js
+++ woowacourse-precourse/javascript-onboarding/week1/PROBLEM1/전세원.js
@@ -1,46 +1,42 @@
-const pobi = [97, 98];
-const crong = [197, 198];
+const pobi = [131, 132];
+const crong = [211, 212];
 
 function solution(pobi, crong) {
-  let answer = [];
+  let answer = "";
   let finalCal = [];
   finalCal.push(cal(pobi), cal(crong));
-  console.log(finalCal);
   if (finalCal[0].includes(0) || finalCal[1].includes(0)) {
-    answer.push(-1);
+    answer = -1;
   } else if (finalCal[0] > finalCal[1]) {
-    answer.push(1);
+    answer = 1;
   } else if (finalCal[0] < finalCal[1]) {
-    answer.push(2);
-  } else answer.push(0);
+    answer = 2;
+  } else {
+    answer = 0;
+  }
   return answer;
 }
 
-function cal(a) {
+function cal(openPage) {
   let maxNum = [];
-  if (a[0] + 1 !== a[1]) {
+  if (openPage[0] + 1 !== openPage[1]) {
     maxNum.push(0);
     return maxNum;
   } else {
     let bothPageMax = []; //각 페이지에서 더한값과 곱한 값 중에 큰 값이 들어간다.
-    for (e of a) {
+    for (page of openPage) {
       //왼쪽 오른쪾 순회 돌면서 계산.
       let pageCal = []; // 각 페이지의 더한 값과 곱한 값이 들어간다.
-      let splitPage = String(e).split(""); // ['9', '6'] ['9', '7']
-      let plusPage = plus(splitPage); //15 16
-      let multiplePage = multiple(splitPage); // 54 63
-      pageCal.push(plusPage, multiplePage); // 15, 54  :: 16 63
-      console.log(`더한값, 곱한값 ${pageCal}`);
-      bothPageMax.push(Math.max(...pageCal)); // 54 , 63
-      console.log(`각 페이지의 최댓값${bothPageMax}`);
+      let splitPage = String(page).split("");
+      let plusPage = plus(splitPage);
+      let multiplePage = multiple(splitPage);
+      pageCal.push(plusPage, multiplePage);
+      bothPageMax.push(Math.max(...pageCal));
     }
-    maxNum.push(Math.max(...bothPageMax)); // 63
-    console.log(`한 인물의 최댓값${maxNum}`);
+    maxNum.push(Math.max(...bothPageMax));
     return maxNum;
   }
 }
-
-// console.log(cal(pobi));
 
 //합하는 함수
 function plus(array) {
@@ -58,5 +54,4 @@ function multiple(array) {
   }
   return result;
 }
-
 console.log(solution(pobi, crong));
