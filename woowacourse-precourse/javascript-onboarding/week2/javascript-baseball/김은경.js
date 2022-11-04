bd926f98bcad379fa60c7fb3968596e04adffd4d
ekkim <eklikeable@kakao.com>
2022-11-04 23:54:52 +0900

    🐛 예외처리시 에러 수정완료

--- woowacourse-precourse/javascript-onboarding/week2/javascript-baseball/김은경.js
+++ woowacourse-precourse/javascript-onboarding/week2/javascript-baseball/김은경.js
@@ -1,4 +1,4 @@
-const MissionUtils = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
   constructor(guessNum) {
@@ -7,7 +7,7 @@ class App {
   generateGuessNum() {
     const computer = [];
     while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      const number = Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
         computer.push(number);
       }
@@ -15,22 +15,19 @@ class App {
     return computer;
   }
   play() {
-    MissionUtils.Console.print(`=== Guess Numbers : ${this.guessNum} ===`);
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다');
-    app.getInput();
+    Console.print('숫자 야구 게임을 시작합니다');
+    this.getInput();
   }
   getInput() {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+    Console.print(`=== 테스트용 정답 : ${this.guessNum} ===`);
+    Console.readLine('숫자를 입력해주세요 : ', (answer) => {
       const userNumbers = answer.split('');
       try {
-        if (this.guessNum.join('') === answer) {
-          this.printWin();
-        } else if (app.checkInput(userNumbers)) {
+        if (this.checkInput(userNumbers)) {
           this.gameProceed(userNumbers, this.guessNum);
         }
       } catch (err) {
-        MissionUtils.Console.print(err);
-        // MissionUtils.Console.close();
+        Console.close();
       }
     });
   }
@@ -54,6 +51,9 @@ class App {
   }
 
   gameProceed(userNumbers, guessNum) {
+    if (this.guessNum.join('') === userNumbers.join('')) {
+      return this.printWin();
+    }
     let strike = 0;
     let ball = 0;
     let cnt = 0;
@@ -70,36 +70,36 @@ class App {
       }
     }
     if (cnt === 3) {
-      MissionUtils.Console.print('낫싱');
-      app.getInput();
+      Console.print('낫싱');
+      this.getInput();
     } else {
-      app.printScore(strike, ball);
-      app.getInput();
+      this.printScore(strike, ball);
+      this.getInput();
     }
   }
 
   printScore(strike, ball) {
     if (strike === 0) {
-      return MissionUtils.Console.print(`${ball}볼`);
+      return Console.print(`${ball}볼`);
     }
     if (ball === 0) {
-      return MissionUtils.Console.print(`${strike}스트라이크`);
+      return Console.print(`${strike}스트라이크`);
     }
-    return MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    return Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 
   printWin() {
-    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    MissionUtils.Console.readLine(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
       (answer) => {
         switch (answer) {
           case '1':
-            app.guessNum = this.generateGuessNum();
-            this.play();
+            this.guessNum = this.generateGuessNum();
+            this.getInput();
             break;
           case '2':
-            MissionUtils.Console.close();
+            Console.close();
             break;
           default:
             this.printWin();
