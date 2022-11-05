48d07c1160294f6e93852ab41416fc847650c148
Chun-gu <chungu.yi92@gmail.com>
2022-11-05 09:00:17 +0900

    ✨ 사용자의 입력에 따라 게임을 재시작 또는 종료하는 함수

---
+++ woowacourse-precourse/javascript-onboarding/week2/javascript-baseball/이춘구/askReplayOrStop.js
@@ -0,0 +1,28 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const { MESSAGE, GAME } = require("./constant");
+const generateComputerAnswer = require("./generateComputerAnswer");
+
+/**
+ * 사용자에게 게임의 재시작 또는 종료 여부를 묻고 분기 처리하는 함수
+ */
+function askReplayOrStop() {
+  Console.print(MESSAGE.win);
+  Console.readLine(MESSAGE.replayOrStop, (answer) => {
+    if (answer === GAME.restart) restartGame();
+    if (answer === GAME.stop) stopGame();
+    if (answer !== GAME.restart && answer !== GAME.stop) throw new Error();
+  });
+}
+
+function restartGame() {
+  const computerAnswer = generateComputerAnswer();
+  playSubroutine(computerAnswer);
+}
+
+function stopGame() {
+  Console.close();
+}
+
+module.exports = askReplayOrStop;
+const playSubroutine = require("./playSubroutine");
