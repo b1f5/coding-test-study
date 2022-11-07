9e80be9b435649e9cd036c7e5a2e429ae736fc15
Chun-gu <chungu.yi92@gmail.com>
2022-11-07 21:57:17 +0900

    ✅ 컴퓨터의 수를 생성하는 함수 테스트 추가

---
+++ woowacourse-precourse/javascript-onboarding/week2/javascript-baseball/이춘구/generateComputerAnswer.test.js
@@ -0,0 +1,9 @@
+const generateComputerAnswer = require("./generateComputerAnswer");
+
+test("컴퓨터의 수를 생성하는 함수 테스트", () => {
+  const result = generateComputerAnswer();
+
+  expect(result).toHaveLength(3);
+  expect(result.join("")).toMatch(/^\d+$/);
+  expect(Array.from(new Set(result))).toHaveLength(3);
+});
