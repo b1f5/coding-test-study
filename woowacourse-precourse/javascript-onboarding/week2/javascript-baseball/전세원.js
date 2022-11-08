837a847946658a8bf525fb69255abe820f2ad20a
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-11-08 16:57:08 +0900

    refactor: 상수로 변경할수 있는 변수 변경, 함수의 기능을 명료하게 하기 위해 return 맞추기, console.log지우기

--- woowacourse-precourse/javascript-onboarding/week2/javascript-baseball/전세원.js
+++ woowacourse-precourse/javascript-onboarding/week2/javascript-baseball/전세원.js
@@ -11,21 +11,21 @@ class App {
 
 function randomNumber() {
   while (computer.length < 3) {
-    const number = Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(parseInt(number, 10));
+    const randomNumberPicked = Random.pickNumberInRange(1, 9);
+    if (!computer.includes(randomNumberPicked)) {
+      computer.push(parseInt(randomNumberPicked, 10));
     }
   }
-  Console.print(computer);
+  // Console.print(computer);
 }
 
 function insertAnswer() {
   Console.readLine("숫자를 맞춰주세요", (answer) => {
-    36;
-    // Console.close();
-    validateInput(answer);
-    let hint = scoreInput(Array.from(answer));
-    if (showHint(hint) === 3) {
+    if (validateInput(answer) === false) {
+      throw "you inserted wrong";
+    }
+    const HINT = scoreInput(Array.from(answer));
+    if (showHint(HINT) === 3) {
       Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       restartOrEnd();
     } else {
@@ -34,20 +34,19 @@ function insertAnswer() {
   });
 }
 
-//input의 길이는 3이어야 하고, 숫자 1-9사이로 이루어져야 한다.
 function validateInput(input) {
-  let regexp = /^[1-9]+$/; //1-9가 들어갔는지 확인
-  if (regexp.test(input) && input.split("").length === 3) {
+  const REGEXP = /^[1-9]+$/; //1-9가 들어갔는지 확인
+  if (REGEXP.test(input) && input.split("").length === 3) {
     return true;
   } else {
-    throw "you inserted wrong";
+    return false;
   }
 }
 
 function scoreInput(answer) {
   let count = { ball: 0, strike: 0, nothing: 0 };
   answer.forEach((element) => {
-    let indexElement = answer.indexOf(element);
+    const indexElement = answer.indexOf(element);
     element = parseInt(element, 10);
     if (element === computer[indexElement]) {
       count.strike += 1;
@@ -61,20 +60,20 @@ function scoreInput(answer) {
 }
 
 function showHint(obj) {
-  let ballNum = Number(obj.ball);
-  let strikeNum = Number(obj.strike);
-  let nothingNum = Number(obj.nothing);
+  const ballNum = Number(obj.ball);
+  const strikeNum = Number(obj.strike);
+  const nothingNum = Number(obj.nothing);
   if (strikeNum === 3) {
-    Console.print("3스트라이트");
+    Console.print("3스트라이크");
     return strikeNum;
   }
-  if (0 < ballNum < 3 && 0 < strikeNum < 3) {
+  if (ballNum !== 0 && strikeNum !== 0) {
     Console.print(`${ballNum}볼 ${strikeNum}스트라이크`);
   }
   if (ballNum !== 0 && strikeNum === 0) {
     Console.print(`${ballNum}볼`);
   }
-  if (ballNum === 0 && 0 < strikeNum < 3) {
+  if (ballNum === 0 && strikeNum !== 0) {
     Console.print(`${strikeNum}스트라이크`);
   }
   if (nothingNum === 3) {
@@ -86,17 +85,17 @@ function restartOrEnd() {
   Console.readLine(
     "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
     (answer) => {
-      let answered = Number(answer);
-      if (answered === 1) {
-        let game = new App();
+      const ANSWERED = Number(answer);
+      if (ANSWERED === 1) {
+        const game = new App();
         game.play();
       }
-      if (answered === 2) {
+      if (ANSWERED === 2) {
         Console.close();
       }
     }
   );
 }
-let gameStart = new App();
-console.log(gameStart.play());
+const gameStart = new App();
+gameStart.play();
 module.exports = App;
