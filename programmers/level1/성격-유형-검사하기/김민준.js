4b11014d704d9d2c30713ff868a294e83b64319f
mj0107 <alswns1285@gmail.com>
2022-11-09 20:23:50 +0900

    ♻️ 명확하게 변수 이용 Refactoring
    - 구조 분해 할당으로 동의와 비동의 나눠줌
    - 배열[인덱스]를 변수에 저장하여 보기 쉽게 바꿔줌

--- programmers/level1/성격-유형-검사하기/김민준.js
+++ programmers/level1/성격-유형-검사하기/김민준.js
@@ -1,7 +1,7 @@
 function solution(survey, choices) {
   let result = '';
 
-  const type = {
+  const TYPE = {
     R: 0,
     T: 0,
     C: 0,
@@ -13,14 +13,17 @@ function solution(survey, choices) {
   };
 
   for (let i = 0; i < survey.length; i += 1) {
-    if (choices[i] > 4) type[survey[i][1]] += choices[i] - 4;
-    else if (choices[i] < 4) type[survey[i][0]] += 4 - choices[i];
+    const [DISAGREE, AGREE] = survey[i];
+    const CHOICE = choices[i];
+
+    if (CHOICE > 4) TYPE[AGREE] += CHOICE - 4;
+    else if (CHOICE < 4) TYPE[DISAGREE] += 4 - CHOICE;
   }
 
-  result += type['R'] >= type['T'] ? 'R' : 'T';
-  result += type['C'] >= type['F'] ? 'C' : 'F';
-  result += type['J'] >= type['M'] ? 'J' : 'M';
-  result += type['A'] >= type['N'] ? 'A' : 'N';
+  result += TYPE['R'] >= TYPE['T'] ? 'R' : 'T';
+  result += TYPE['C'] >= TYPE['F'] ? 'C' : 'F';
+  result += TYPE['J'] >= TYPE['M'] ? 'J' : 'M';
+  result += TYPE['A'] >= TYPE['N'] ? 'A' : 'N';
 
   return result;
 }
