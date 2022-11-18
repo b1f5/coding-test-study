3b0aeadb4c3efcd30e076fc4b67a01ffb09deb77
ekkim <eklikeable@kakao.com>
2022-11-19 00:03:00 +0900

    🎨 햄버거 만들기

--- programmers/level1/햄버거-만들기/김은경.js
+++ programmers/level1/햄버거-만들기/김은경.js
@@ -1,11 +1,23 @@
-function solution(ingredient) {
-  const pattern = /1231/;
-  let order = ingredient.join('');
-  let count = 0;
+// slice(a, b) : 인덱스 a 부터 b-1 까지 잘라낸 배열을 반환
+// splice(a, b) : 기존배열에서 인덱스 a 부터 b개의 요소를 삭제한 결과를 반환
 
-  while (pattern.test(order)) {
-    order = order.replace(pattern, '');
-    count = count + 1;
+function solution(ingredient) {
+  let count = 0;
+  const stack = [];
+
+  for (let i = 0; i < ingredient.length; i++) {
+    stack.push(ingredient[i]);
+    if (stack.length < 4) continue;
+
+    if (ingredient[i] === 1) {
+      // 4번째 이후 들어온 재료가 빵인경우, 스택에 직전에 들어온 재료 3가지를 확인
+      const breadOnTopIdx = stack.length - 1;
+      const burger = stack.slice(breadOnTopIdx - 3, breadOnTopIdx + 1).join('');
+      if (burger === '1231') {
+        stack.splice(breadOnTopIdx - 3, 4); // 재료순서가 맞으면 스택에서 burger 삭제
+        count += 1;
+      }
+    }
   }
   return count;
 }
