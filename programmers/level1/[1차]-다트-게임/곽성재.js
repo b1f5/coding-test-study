a37c835fda624e51a5d787c7fbf66a59dba73bcf
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-25 23:31:03 +0900

    ♻️ 코드길이 줄여보기

--- programmers/level1/[1차]-다트-게임/곽성재.js
+++ programmers/level1/[1차]-다트-게임/곽성재.js
@@ -5,8 +5,11 @@ function solution(dartResult) {
   const BONUS = { S: 1, D: 2, T: 3 };
 
   for (let i = 0; i < dartResult.length; i++) {
+    // 만약 [S, D, T]가 -를 포함한다면
     if (Object.keys(BONUS).includes(dartResult[i])) {
+      // S D T
       const bonus = dartResult[i];
+      // 점수가 10인 경우
       if (dartResult[i - 2] === "1") {
         temp.push(Math.pow(10, BONUS[bonus]));
       } else {
@@ -19,39 +22,26 @@ function solution(dartResult) {
   // 첫번째 기회의 옵션 => 무조건 인덱스 2
   // 두번째 기회의 옵션 => 인덱스 4(첫번째가 있을때) 또는 인덱스 5(첫번째가 없을때)
   // 세번째 기회의 옵션 => 인덱스 6(앞서서 하나도 없을때) 또는 인덱스 7(앞서서 하나 있을때) 또는 인덱스 8(앞서서 둘다 있을때)
+  // Map
+  const RANGE = {
+    2: 0,
+    4: 1,
+    5: 1,
+    6: 2,
+    7: 2,
+    8: 2,
+  };
+  const OPTION = { "#": -1, "*": 2 };
   for (let i = 0; i < dartResult.length; i++) {
     const option = dartResult[i];
     if (option === "#") {
-      switch (i) {
-        case 2:
-          temp[0] = -temp[0];
-          break;
-        case 4:
-        case 5:
-          temp[1] = -temp[1];
-          break;
-        case 6:
-        case 7:
-        case 8:
-          temp[2] = -temp[2];
-          break;
-      }
+      temp[RANGE[i]] = -temp[RANGE[i]];
     } else if (option === "*") {
-      switch (i) {
-        case 2:
-          temp[0] = 2 * temp[0];
-          break;
-        case 4:
-        case 5:
-          temp[0] = 2 * temp[0];
-          temp[1] = 2 * temp[1];
-          break;
-        case 6:
-        case 7:
-        case 8:
-          temp[1] = 2 * temp[1];
-          temp[2] = 2 * temp[2];
-          break;
+      if (RANGE[i] === 0) {
+        temp[RANGE[i]] = 2 * temp[RANGE[i]];
+      } else if (RANGE[i] !== 0) {
+        temp[RANGE[i] - 1] = 2 * temp[RANGE[i] - 1];
+        temp[RANGE[i]] = 2 * temp[RANGE[i]];
       }
     }
   }
@@ -65,38 +55,3 @@ console.log(solution("1S*2T*3S"));
 console.log(solution("1D#2S*3S"));
 console.log(solution("1T2D3D#"));
 console.log(solution("1D2S3T*"));
-
-/**
-테스트 1 〉	통과 (0.13ms, 33.4MB)
-테스트 2 〉	통과 (0.12ms, 33.5MB)
-테스트 3 〉	통과 (0.13ms, 33.6MB)
-테스트 4 〉	통과 (0.12ms, 33.5MB)
-테스트 5 〉	통과 (0.12ms, 33.4MB)
-테스트 6 〉	통과 (0.13ms, 33.4MB)
-테스트 7 〉	통과 (0.13ms, 33.4MB)
-테스트 8 〉	통과 (0.13ms, 33.6MB)
-테스트 9 〉	통과 (0.13ms, 33.4MB)
-테스트 10 〉	통과 (0.14ms, 33.5MB)
-테스트 11 〉	통과 (0.14ms, 33.5MB)
-테스트 12 〉	통과 (0.12ms, 33.4MB)
-테스트 13 〉	통과 (0.18ms, 33.5MB)
-테스트 14 〉	통과 (0.14ms, 33.5MB)
-테스트 15 〉	통과 (0.13ms, 33.4MB)
-테스트 16 〉	통과 (0.13ms, 33.4MB)
-테스트 17 〉	통과 (0.22ms, 33.6MB)
-테스트 18 〉	통과 (0.17ms, 33.5MB)
-테스트 19 〉	통과 (0.13ms, 33.5MB)
-테스트 20 〉	통과 (0.14ms, 33.4MB)
-테스트 21 〉	통과 (0.20ms, 33.2MB)
-테스트 22 〉	통과 (0.18ms, 33.4MB)
-테스트 23 〉	통과 (0.19ms, 33.5MB)
-테스트 24 〉	통과 (0.18ms, 33.4MB)
-테스트 25 〉	통과 (0.15ms, 33.5MB)
-테스트 26 〉	통과 (0.21ms, 33.4MB)
-테스트 27 〉	통과 (0.22ms, 33.4MB)
-테스트 28 〉	통과 (0.13ms, 33.6MB)
-테스트 29 〉	통과 (0.15ms, 33.4MB)
-테스트 30 〉	통과 (0.13ms, 33.4MB)
-테스트 31 〉	통과 (0.13ms, 33.5MB)
-테스트 32 〉	통과 (0.15ms, 33.5MB)
- */
