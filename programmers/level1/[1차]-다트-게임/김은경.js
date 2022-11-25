ff3984b30bb20791471521331525b5f54cfb6f82
ekkim <eklikeable@kakao.com>
2022-11-25 15:36:17 +0900

    refactor : 점수와 옵션을 객체로 분리하여 중복 코드 최적화

--- programmers/level1/[1차]-다트-게임/김은경.js
+++ programmers/level1/[1차]-다트-게임/김은경.js
@@ -1,78 +1,45 @@
 function solution(dartResult) {
-  const pattern = /[0-9]+[STD][*#]?/g;
-  const array = dartResult.match(pattern);
-  const score = [];
+  const pattern = /([0-9]+)([STD])([*#]?)/g;
+  const scores = dartResult.match(pattern);
 
-  array.forEach((el) => {
-    const individual = el.split('');
-    let divider = individual[individual.length - 1];
-    if (divider === 'S' || divider === 'T' || divider === 'D') {
-      const gottonNum = parseInt(individual.slice(0, -1).join(''));
-      let getScore = 0;
-      switch (divider) {
-        case 'S':
-          getScore = gottonNum;
-          break;
-        case 'D':
-          getScore = Math.pow(gottonNum, 2);
-          break;
-        case 'T':
-          getScore = Math.pow(gottonNum, 3);
-          break;
-      }
-      score.push(getScore);
+  const BONUS = { S: 1, D: 2, T: 3 };
+  const BONUSES = Object.keys(BONUS);
+  const OPTION = { '*': 2, '#': -1 };
+  const OPTIONS = Object.keys(OPTION);
+
+  const totalScore = [];
+
+  scores.forEach((result) => {
+    const individual = result.split('');
+    const divider = individual[individual.length - 1];
+    let getScore = 0;
+
+    if (BONUSES.includes(divider)) {
+      const score = parseInt(individual.slice(0, -1).join(''));
+      getScore = Math.pow(score, BONUS[divider]);
     }
-    if (divider === '*') {
-      score[score.length - 1] *= 2;
-      let divider = individual[individual.length - 2];
-      const gottonNum = parseInt(individual.slice(0, -2).join(''));
-      let getScore = 0;
-      switch (divider) {
-        case 'S':
-          getScore = 2 * gottonNum;
-          break;
-        case 'D':
-          getScore = 2 * Math.pow(gottonNum, 2);
-          break;
-        case 'T':
-          getScore = 2 * Math.pow(gottonNum, 3);
-          break;
-      }
-      score.push(getScore);
+    if (OPTIONS.includes(divider)) {
+      if (divider === '*') totalScore[totalScore.length - 1] *= 2;
+      const bonus = individual[individual.length - 2];
+      const score = parseInt(individual.slice(0, -2).join(''));
+      getScore = OPTION[divider] * Math.pow(score, BONUS[bonus]);
     }
-    if (divider === '#') {
-      let divider = individual[individual.length - 2];
-      const gottonNum = parseInt(individual.slice(0, -2).join(''));
-      let getScore = 0;
-      switch (divider) {
-        case 'S':
-          getScore = -1 * gottonNum;
-          break;
-        case 'D':
-          getScore = -1 * Math.pow(gottonNum, 2);
-          break;
-        case 'T':
-          getScore = -1 * Math.pow(gottonNum, 3);
-          break;
-      }
-      score.push(getScore);
-    }
+    totalScore.push(getScore);
   });
-
-  return score.reduce((acc, num) => acc + num, 0);
+  return totalScore.reduce((acc, num) => acc + num, 0);
 }
 
-// // 37
-// console.log(solution('1S2D*3T'));
-// // 9
-// console.log(solution('1D2S#10S'));
-// // 3
-// console.log(solution('1D2S0T'));
-// // 23
-// console.log(solution('1S*2T*3S'));
-// // 5
-// console.log(solution('1D#2S*3S	'));
-// // -4
-// console.log(solution('1T2D3D#'));
-// // 59
-// console.log(solution('1D2S3T*'));
+// 37
+console.log(solution('1S2D*3T'));
+// 9
+console.log(solution('1D2S#10S'));
+// 3
+console.log(solution('1D2S0T'));
+// 23
+console.log(solution('1S*2T*3S'));
+// 5
+console.log(solution('1D#2S*3S	'));
+// -4
+console.log(solution('1T2D3D#'));
+// 59
+console.log(solution('1D2S3T*'));
