ed43014d2daa43ec387949b015648961bd98f078
mj0107 <alswns1285@gmail.com>
2022-11-25 22:48:35 +0900

    🎨 bonus와 option 객체로 묶어서 처리하기

--- programmers/level1/[1차]-다트-게임/김민준.js
+++ programmers/level1/[1차]-다트-게임/김민준.js
@@ -1,30 +1,33 @@
 function solution(dartResult) {
-  let answer = 0;
+  const BONUS = { S: 1, D: 2, T: 3 };
+  const OPTION = { '#': -1, '*': 2 };
   let [prevScore, score] = [0, 0];
+  let answer = 0;
 
   for (let i = 0; i < dartResult.length; i += 1) {
-    let dart = dartResult[i];
+    const DART = dartResult[i];
 
-    if ('0' <= dart && dart <= '9') {
+    if ('0' <= DART && DART <= '9') {
       prevScore = score;
       // 점수가 10점일때
       if (dartResult[i + 1] === '0') {
         score = 10;
         i += 1;
-      } else score = parseInt(dart);
-    } else if (dart === 'S' || 'D' || 'T') {
-      // 'S'는 생략 가능
-      if (dart === 'S') score = score ** 1;
-      else if (dart === 'D') score = score ** 2;
-      else if (dart === 'T') score = score ** 3;
+      } 
+      else score = parseInt(DART);
+    } 
+    
+    // bonus
+    else if (DART === 'S' || DART === 'D' || DART === 'T') {
+      score = score ** BONUS[DART];
 
-      if (dartResult[i + 1] === '*') {
-        // 이미 answer에 더해져있기 때문에 한번만 더함
-        answer += prevScore;
-        score *= 2;
-        i += 1;
-      } else if (dartResult[i + 1] === '#') {
-        score *= -1;
+      // option
+      if(dartResult[i + 1] === '*' || dartResult[i + 1] === '#') {
+        if (dartResult[i + 1] === '*') {
+          // 이미 answer에 더해져있기 때문에 한번만 더함
+          answer += prevScore;
+        }
+        score *= OPTION[dartResult[i + 1]];
         i += 1;
       }
 
