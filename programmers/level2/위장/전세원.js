c9e16a0173662bb0787f5e97e288c9a3c6bdee3d
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-11-30 17:06:29 +0900

    ✅위장

--- programmers/level2/위장/전세원.js
+++ programmers/level2/위장/전세원.js
@@ -5,75 +5,91 @@
 //나온 조합끼리 곱해서.. 다 더한다.
 
 const clothes = [
-  ["crow_mask", "face"],
-  ["blue_sunglasses", "face"],
-  ["smoky_makeup", "face"],
+  ["yellow_hat", "headgear"],
+  ["blue_sunglasses", "eyewear"],
+  ["green_turban", "headgear"],
 ];
 
+// function solution(clothes) {
+//   var answer = 0;
+//   //옷 종류만 담겨있음.
+//   const typeOfClothes = [];
+//   for (let i = 0; i < clothes.length; i++) {
+//     const [name, type] = clothes[i];
+//     typeOfClothes.push(type);
+//   }
+
+//   //갯수 세주는 함수
+//   const typesAndNumbers = clothes.reduce(
+//     (typesAndNumbers, [_, clothesType]) => {
+//       // 현재 카테고리에 의상이 없으면 0 있으면 1을 더한다.
+//       typesAndNumbers[clothesType] = (typesAndNumbers[clothesType] || 0) + 1;
+//       return typesAndNumbers;
+//     },
+//     {}
+//   );
+//   console.log(typesAndNumbers);
+//   // 옷 종류 : 그 옷 종류가 몇벌 있는지를 담고 있는 객체
+
+//   const type = Object.keys(typesAndNumbers);
+//   //옷 종류 갯수
+
+//   //배열중 몇개 뽑을지 정해주면 조합 뽑아주는 함수
+//   const getCombinations = function (arr, selectNumber) {
+//     const results = [];
+//     if (selectNumber === 1) return arr.map((el) => [el]);
+//     // n개중에서 1개 선택할 때(nC1), 바로 모든 배열의 원소 return
+
+//     arr.forEach((fixed, index, origin) => {
+//       const rest = origin.slice(index + 1);
+//       // 해당하는 fixed를 제외한 나머지 뒤
+//       const combinations = getCombinations(rest, selectNumber - 1);
+//       // 나머지에 대해서 조합을 구한다.
+//       const attached = combinations.map((el) => [fixed, ...el]);
+//       //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+//       results.push(...attached);
+//       // 배열 spread syntax 로 모두다 push
+//     });
+
+//     return results; // 결과 담긴 results return
+//   };
+
+//   const combinationArray = [];
+//   //모든 조합들이 다 감기는 배열
+//   for (let i = 1; i <= type.length; i++) {
+//     combinationArray.push(...getCombinations(type, i));
+//     //(2, 1)(2, 2) -> 이렇게 구함
+//   }
+//   console.log(combinationArray);
+
+//   combinationArray.forEach((array) => {
+//     console.log(array);
+//     let multiple = array.reduce(
+//       (total, val) => total * typesAndNumbers[val],
+//       1
+//     );
+//     //순회를 돌며 각 배열안에 있는 옷종류를 typesAndNumbers에서 value를 찾아서 곱해준다.
+//     console.log(multiple);
+//     answer += multiple;
+//   });
+//   return answer;
+// }
+
 function solution(clothes) {
-  var answer = 0;
-  //옷 종류만 담겨있음.
-  const typeOfClothes = [];
-  for (let i = 0; i < clothes.length; i++) {
-    const [name, type] = clothes[i];
-    typeOfClothes.push(type);
-  }
-
-  //갯수 세주는 함수
-  const reducer = function (accumulator, value, index, array) {
-    if (accumulator.hasOwnProperty(value)) {
-      accumulator[value] = accumulator[value] + 1;
-    } else {
-      accumulator[value] = 1;
-    }
-    return accumulator;
-  };
-  const initialValue = {};
-  const typesAndNumbers = typeOfClothes.reduce(reducer, initialValue);
+  let answer = 0;
+  const typesAndNumbers = clothes.reduce(
+    (typesAndNumbers, [_, clothesType]) => {
+      // 현재 카테고리에 의상이 없으면 0 있으면 1을 더한다.
+      typesAndNumbers[clothesType] = (typesAndNumbers[clothesType] || 1) + 1;
+      return typesAndNumbers;
+    },
+    {}
+  );
   console.log(typesAndNumbers);
-  // 옷 종류 : 그 옷 종류가 몇벌 있는지를 담고 있는 객체
-  let type = Object.keys(typesAndNumbers);
-  //옷 종류 갯수
 
-  //배열중 몇개 뽑을지 정해주면 조합 뽑아주는 함수
-  const getCombinations = function (arr, selectNumber) {
-    const results = [];
-    if (selectNumber === 1) return arr.map((el) => [el]);
-    // n개중에서 1개 선택할 때(nC1), 바로 모든 배열의 원소 return
-
-    arr.forEach((fixed, index, origin) => {
-      const rest = origin.slice(index + 1);
-      // 해당하는 fixed를 제외한 나머지 뒤
-      const combinations = getCombinations(rest, selectNumber - 1);
-      // 나머지에 대해서 조합을 구한다.
-      const attached = combinations.map((el) => [fixed, ...el]);
-      //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
-      results.push(...attached);
-      // 배열 spread syntax 로 모두다 push
-    });
-
-    return results; // 결과 담긴 results return
-  };
-
-  const combinationArray = [];
-  //모든 조합들이 다 감기는 배열
-  for (let i = 1; i <= type.length; i++) {
-    combinationArray.push(...getCombinations(type, i));
-    //(2, 1)(2, 2) -> 이렇게 구함
-  }
-  console.log(combinationArray);
-
-  combinationArray.forEach((array) => {
-    console.log(array);
-    let multiple = array.reduce(
-      (total, val) => total * typesAndNumbers[val],
-      1
-    );
-    //순회를 돌며 각 배열안에 있는 옷종류를 typesAndNumbers에서 value를 찾아서 곱해준다.
-    console.log(multiple);
-    answer += multiple;
-  });
+  const amountOfClothes = Object.values(typesAndNumbers);
+  console.log(amountOfClothes);
+  answer += amountOfClothes.reduce((acc, cur) => acc * cur) - 1;
   return answer;
 }
-
 console.log(solution(clothes));
