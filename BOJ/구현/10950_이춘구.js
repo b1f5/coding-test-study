331e14443c3e91f03cabbfd517c8f0412ca075eb
Chun-gu <chungu.yi92@gmail.com>
2023-03-28 00:05:44 +0900

    ✨ 10950

--- BOJ/구현/10950_이춘구.js
+++ BOJ/구현/10950_이춘구.js
@@ -1,22 +1,30 @@
-const input = `5
-1 1
-2 3
-3 4
-9 8
-5 2`
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
   .trim()
-  .split(" ")
-  .map(Number);
+  .split("\n")
+  .slice(1);
 
-// const input = require("fs")
-//   .readFileSync("/dev/stdin")
+// const input = `5
+// 1 1
+// 2 3
+// 3 4
+// 9 8
+// 5 2`
 //   .toString()
 //   .trim()
-//   .split(" ")
-//   .map(Number);
+//   .split("\n")
+//   .slice(1);
 
 solution(input);
 
-function solution(input){
-  
+function solution(input) {
+  const answer = [];
+
+  input.forEach((inp) => {
+    const [A, B] = inp.split(" ").map(Number);
+    answer.push(A + B);
+  });
+
+  console.log(answer.join("\n"));
 }
