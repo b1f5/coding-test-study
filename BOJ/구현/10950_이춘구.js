3d12135cdaa57bad4639483cda7b651395d71f6e
Chun-gu <chungu.yi92@gmail.com>
2023-03-27 23:59:41 +0900

    ✨ 10950

---
+++ BOJ/구현/10950_이춘구.js
@@ -0,0 +1,22 @@
+const input = `5
+1 1
+2 3
+3 4
+9 8
+5 2`
+  .trim()
+  .split(" ")
+  .map(Number);
+
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .trim()
+//   .split(" ")
+//   .map(Number);
+
+solution(input);
+
+function solution(input){
+  
+}
