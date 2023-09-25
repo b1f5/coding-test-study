ef333ba7785cb4a505b822fa58212ccf90d6d440
Chun-gu <chungu.yi92@gmail.com>
2023-09-25 16:11:23 +0900

    Queue 구현으로 개선

--- BOJ/Graph/2178_이춘구.js
+++ BOJ/Graph/2178_이춘구.js
@@ -1,42 +1,68 @@
-const input = require("fs")
+const [NM, ...rest] = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n");
 
-let [NM, ...rest] = input;
-const [destR, destC] = NM.split(" ").map((v) => Number(v) - 1);
-const graph = rest.map((e) => e.split("").map(Number));
+class Queue {
+  #queue = {};
+  #head = 0;
+  #tail = 1;
 
-let needVisit = [[0, 0]];
+  push(value) {
+    this.#queue[this.#tail] = value;
+    this.#tail += 1;
+  }
 
-const directions = [
-  [1, 0],
-  [-1, 0],
-  [0, 1],
-  [0, -1],
-];
+  shift() {
+    if (this.length() === 0) return undefined;
 
-while (needVisit.length > 0) {
-  const [r, c] = needVisit.shift();
+    const value = this.#queue[this.#head + 1];
+    delete this.#queue[this.#head + 1];
+    this.#head += 1;
+    if (this.#tail - this.#head === 1) {
+      this.#head = 0;
+      this.#tail = 1;
+    }
 
-  if (r === destR && c === destC) break;
+    return value;
+  }
+
+  length() {
+    return this.#tail - this.#head - 1;
+  }
+}
+
+const [N, M] = NM.split(" ").map(Number);
+const maze = rest.map((v) => v.split(""));
+
+// prettier-ignore
+const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+const visited = Array.from({ length: N }, () => new Array(M).fill(false));
+const willVisit = new Queue();
+willVisit.push([0, 0, 1]);
+
+while (willVisit.length) {
+  const [r, c, answer] = willVisit.shift();
+
+  if (r === N - 1 && c === M - 1) {
+    return console.log(answer);
+  }
 
   for (const [dR, dC] of directions) {
-    const newR = r + dR;
-    const newC = c + dC;
-    const isInRange =
-      newR >= 0 &&
-      newR <= destR &&
-      newC >= 0 &&
-      newC <= destC &&
-      graph[newR][newC] === 1;
+    const [newR, newC] = [r + dR, c + dC];
 
-    if (isInRange) {
-      graph[newR][newC] = graph[r][c] + 1;
-      needVisit.push([newR, newC]);
+    if (
+      isInMaze(newR, newC) &&
+      maze[newR][newC] === "1" &&
+      !visited[newR][newC]
+    ) {
+      visited[newR][newC] = true;
+      willVisit.push([newR, newC, answer + 1]);
     }
   }
 }
 
-console.log(graph[destR][destC]);
+function isInMaze(r, c) {
+  return 0 <= r && r < N && 0 <= c && c < M;
+}
